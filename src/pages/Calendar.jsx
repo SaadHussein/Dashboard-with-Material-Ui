85bcda4ebd125ce5f784fd74f82c40665bc9707a
SaadHussein <sa3dhusseein@gmail.com>
2023-09-07 18:02:15 +0300

    Make Calender Page and Finish it

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,105 +1,105 @@
-// import { useState } from "react";
-// import { formatDate } from "@fullcalendar/core";
-// import FullCalendar from "@fullcalendar/react";
-// import dayGridPlugin from "@fullcalendar/daygrid";
-// import timeGridPlugin from "@fullcalendar/timegrid";
-// import interactionPlugin from "@fullcalendar/interaction";
-// import { Paper, Stack } from "@mui/material";
-// import "./Calendar.css";
+import { useState } from "react";
+import { formatDate } from "@fullcalendar/core";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { Paper, Stack } from "@mui/material";
+import "./Calendar.css";
 
-// function renderEventContent(eventInfo) {
-//   return (
-//     <>
-//       <b>{eventInfo.timeText}</b>
-//       <i>{eventInfo.event.title}</i>
-//     </>
-//   );
-// }
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
+}
 
-// function renderSidebarEvent(event) {
-//   return (
-//     <li key={event.id}>
-//       <b>
-//         {formatDate(event.start, {
-//           year: "numeric",
-//           month: "short",
-//           day: "numeric",
-//         })}
-//       </b>
-//       <i>{event.title}</i>
-//     </li>
-//   );
-// }
+function renderSidebarEvent(event) {
+  return (
+    <li key={event.id}>
+      <b>
+        {formatDate(event.start, {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}
+      </b>
+      <i>{event.title}</i>
+    </li>
+  );
+}
 
-// const Calendar = () => {
-//   const [currentEvents, setCurrentEvents] = useState([]);
+const Calendar = () => {
+  const [currentEvents, setCurrentEvents] = useState([]);
 
-//   let eventGuid = 0;
-//   function createEventId() {
-//     return String(eventGuid++);
-//   }
+  let eventGuid = 0;
+  function createEventId() {
+    return String(eventGuid++);
+  }
 
-//   const handleDateSelect = (selectInfo) => {
-//     let title = prompt("Please enter a new title for your event");
-//     let calendarApi = selectInfo.view.calendar;
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
 
-//     calendarApi.unselect(); // clear date selection
+    calendarApi.unselect(); // clear date selection
 
-//     if (title) {
-//       calendarApi.addEvent({
-//         id: createEventId(),
-//         title,
-//         start: selectInfo.startStr,
-//         end: selectInfo.endStr,
-//         allDay: selectInfo.allDay,
-//       });
-//     }
-//   };
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
 
-//   const handleEventClick = (clickInfo) => {
-//     if (
-//       confirm(
-//         `Are you sure you want to delete the event '${clickInfo.event.title}'`
-//       )
-//     ) {
-//       clickInfo.event.remove();
-//     }
-//   };
+  const handleEventClick = (clickInfo) => {
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  };
 
-//   const handleEvents = (events) => {
-//     setCurrentEvents(events);
-//   };
+  const handleEvents = (events) => {
+    setCurrentEvents(events);
+  };
 
-//   return (
-//     <Stack direction={"row"}>
-//       <Paper className="demo-app-sidebar-section">
-//         <h2 style={{ textAlign: "center" }}>
-//           All Events ({currentEvents.length})
-//         </h2>
-//         <ul>{currentEvents.map(renderSidebarEvent)}</ul>
-//       </Paper>
-//       <div className="demo-app-main">
-//         <FullCalendar
-//           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-//           headerToolbar={{
-//             left: "prev,next today",
-//             center: "title",
-//             right: "dayGridMonth,timeGridWeek,timeGridDay",
-//           }}
-//           initialView="dayGridMonth"
-//           editable={true}
-//           selectable={true}
-//           selectMirror={true}
-//           dayMaxEvents={true}
-//           weekends={true}
-//           select={handleDateSelect}
-//           eventContent={renderEventContent}
-//           eventClick={handleEventClick}
-//           eventsSet={handleEvents}
-//         />
-//       </div>
-//     </Stack>
-//   );
-// };
+  return (
+    <Stack direction={"row"}>
+      <Paper className="demo-app-sidebar-section">
+        <h2 style={{ textAlign: "center" }}>
+          All Events ({currentEvents.length})
+        </h2>
+        <ul>{currentEvents.map(renderSidebarEvent)}</ul>
+      </Paper>
+      <div className="demo-app-main">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={true}
+          select={handleDateSelect}
+          eventContent={renderEventContent}
+          eventClick={handleEventClick}
+          eventsSet={handleEvents}
+        />
+      </div>
+    </Stack>
+  );
+};
 
-// export default Calendar;
+export default Calendar;
